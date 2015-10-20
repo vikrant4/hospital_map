d65604235d2c2429860184550a7a8e455be9ae6a
Vikrant Yadav <vikrant.y.04@gmail.com>
2015-10-20 18:35:35 +0530

    Correcion

--- hospital_map.R
+++ hospital_map.R
@@ -1,6 +1,10 @@
 library(ggmap)
 library(ggplot2)
-hospitals <- reac.csv("datafile.csv")
+if(!file.exists("datafile.csv")){
+  file.create("datafile.csv")
+  download.file("https://data.gov.in/node/356921/datastore/export/csv", "datafile.csv")
+}
+hospitals <- read.csv("datafile.csv")
 colnames(hospitals)[22] <- "lat"
 colnames(hospitals)[23] <- "lon"
 hospitals$lon <- gsub("'", "", hospitals$lon)
@@ -12,7 +16,9 @@ hospitals$lat <- gsub("Â",".", hospitals$lat)
 hospitals$lat <- as.numeric(hospitals$lat)
 hospitals$lon <- as.numeric(hospitals$lon)
 p <- qmap("India", zoom = 5, maptype = "toner")
-png("hospital_map.png")
-p + geom_point(aes(x = lon, y = lat, color = Hospital.Category, size = 4, alpha = 0.6),
+p <- p + geom_point(aes(x = lon, y = lat, color = Hospital.Category, size = 4, alpha = 0.6),
                pch = 19, data = hospitals) + guides(alpha = FALSE)
+
+png("hospital_map.png", width = 720, height = 720)
+print(p)
 dev.off()
